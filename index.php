02ce18476d2a34b16cc360a24fc63aa006fa5d4d
zooz3456 <zooz0806@gmail.com>
2018-06-17 19:04:17 +0900

    git push 연습입니닥

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<?php get_header();?>
+
+<h3> 워드 프레스 테마 만들기 강좌 <h3>
+
+<h1> wordpress theme webcreatvi<h1>
+<h2> wordpress theme webcreatvi<h2>
+<h3> wordpress theme webcreatvi<h3>
+<h4> wordpress theme webcreatvi<h4>
